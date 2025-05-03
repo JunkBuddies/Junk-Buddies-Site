f9b385639e3454fb313702ed8b0e2edcad9240b9
JunkBuddies <JunkBuddies.info@gmail.com>
2025-05-03 17:14:52 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'xs': '480px', // Adds xs breakpoint for small devices
+        'xs': '480px', // Extra small devices (like small mobile)
+        // Keep default Tailwind breakpoints intact
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
     },
   },
