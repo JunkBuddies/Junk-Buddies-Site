ae2a82710132994954d2f9882bcaaea1b50772d3
JunkBuddies <JunkBuddies.info@gmail.com>
2025-08-12 02:39:49 -0700

    Update smart-selector.js

--- api/smart-selector.js
+++ api/smart-selector.js
@@ -1,7 +1,7 @@
 // /api/smart-selector.js
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method === 'GET') {
     return res.status(200).send('ok');
   }
   return res.status(200).json({ reply: 'ok' });
-};
+}
