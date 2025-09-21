c22a3ee08f5e777d554c544d7becdabe92f331a0
JunkBuddies <JunkBuddies.info@gmail.com>
2025-09-21 00:32:34 -0700

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -52,7 +52,6 @@ function LandingPage() {
   return (
     <div className="w-full overflow-hidden">
       <div className="hero-earth-bg w-full min-h-screen flex flex-col items-center justify-center text-white text-center relative">
-        />
 
         <div className="absolute top-6 right-6 z-50">
           <Link
