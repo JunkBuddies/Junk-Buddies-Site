42821d5486b106797b5ac5b407aef572bc123231
JunkBuddies <JunkBuddies.info@gmail.com>
2025-09-21 17:00:33 -0700

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -160,7 +160,7 @@ function LandingPage() {
           </button>
         </div>
       </div>
-       </div>
+    
       {/* REQUIRE SERVICE TODAY BAR */}
       <div className="w-full text-center text-lg text-white py-10 px-6 about-reveal silver">
         <p className="text-xl mb-4">
